cf09d5f84a086c0eaa88bdbc83e3c2732c4fab44
wonho <wonest0918@estsoft.com>
2017-12-19 11:46:28 +0900

    add more hello world

--- src/main.java
+++ src/main.java
@@ -2,5 +2,8 @@ public class main {
 
     public static void main(String[] args) {
         System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
     }
 }
